7e59c99a3e59439b100f61e0aadf5d78b577d8d4
AndreRibeiro24 <andrebecas20@gmail.com>
2026-03-26 20:45:13 +0000

    Day 1

--- src/question.js
+++ src/question.js
@@ -2,6 +2,20 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+    constructor(text,choices,answer,difficulty){
+        this.text = text
+        this.choices = choices 
+        this.answer = answer
+        this.difficulty = difficulty
+    }
+    shuffleChoices(){
+        let randomChoice = [];
+        for(let i = this.choices.length - 1; i > 0; i--  ){
+            const randomizer = Math.floor(Math.random()* (i+1));
+            randomChoice.push([this.choices[i], this.choices[randomizer]] = [this.choices[randomizer], this.choices[i]])
+        }
+        return randomChoice;
+    }
 }
+
+// Done for now 
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,43 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
+   
+    getQuestion(){
+      return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        return this.currentQuestionIndex += 1;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        let randomQuestion = [];
+        for(let i=this.questions.length - 1; i > 0; i--){
+            const randomizer = Math.floor(Math.random()*(i+1));
+            randomQuestion.push([this.questions[i],this.questions[randomizer]]=[this.questions[randomizer],this.questions[i]])
+        }
+        return randomQuestion;
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        if(answer === this.getQuestion().answer){
+            this.correctAnswers += 1;
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded(){
+        if(this.currentQuestionIndex < this.questions.length){
+            return false
+        }else if (this.currentQuestionIndex === this.questions.length){
+            return true
+        }
+
+    }
 }
